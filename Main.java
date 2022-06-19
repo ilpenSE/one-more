ee56d62568dda1622a38684cce41f70db9022c9e
Sadece Emir <86079863+Sadece-Emir@users.noreply.github.com>
2022-06-19 10:10:17 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,14 +7,26 @@ public class Main {
   }
   
   public static boolean equalsOneMore(Object query, Object... vals) { 
-   return Arrays.toString(vals).contains((CharSequence) query);
+    return Arrays.toString(vals).contains((CharSequence) query);
   }
   
   public static boolean containsOneMore(String query,String... values) {
-        int i = 0;
-        for (String s : values) {
-            i += query.contains(s) ? 1 : 0;
-        }
-        return i > 0;
+    int i = 0;
+    for (String s : values) {
+      i += query.contains(s) ? 1 : 0;
     }
+    return i > 0;
+  }
+  
+  public static boolean equalsIgnoreCaseOneMore(Object query,Object... values) {
+    return Arrays.toString(values).toLowerCase().contains(((CharSequence) query).toLowerCase());
+  }
+    
+  public static boolean containsIgnoreCaseOneMore(String query,String... values) {
+    int i = 0;
+    for (String s : values) {
+      i += query.toLowerCase().contains(s.toLowerCase()) ? 1 : 0;
+    }
+    return i > 0;
+  }
 }
